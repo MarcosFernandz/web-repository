dfbf22e6a616845f0458afa2278428abfceeeeb5
Marcos Fernandz <marcosfernandezgomez@gmail.com>
2023-04-28 10:40:10 +0200

    Nuevo-repositorio1lab3a

---
+++ Sections/js/ejercicios/ejercicio001/index.js
@@ -0,0 +1,44 @@
+console.log("work here")
+const movies = [
+    {
+      movie: "Wolf of Wallstreet",
+      genre: ["Drama", "Action", "Comedy"],
+      director: ["Martin Scorsese"],
+      location: "New York City, NY",
+      actors: ["Jonah Hill", "Margo Robbie", "Leo Di Caprio"],
+      year: 2010,
+    },
+    {
+      movie: "Fight Club",
+      genre: ["Drama", "Noir", "Comedy", "Action"],
+      director: ["David Fincher"],
+      location: "Los Angeles, California",
+      actors: ["Bradd Pitt", "Edward Norton", "Helena Bonham Carter"],
+      year: 1999,
+    },
+    {
+      movie: "The Neon Demon",
+      genre: ["Aleix", "Thriller", "Drama"],
+      director: ["Nicolas Winding Refn"],
+      location: "Copenhagen, Denmark",
+      actors: ["Elle Fanning", "Jenna Malone", "Keanu Reeves"],
+      year: 2016,
+    },
+  ];
+
+
+
+console.log(movies[1].director[0]);
+console.log(movies[1].movie);
+
+
+//VIEJA ESCUELA - MEZCLANDO STRINGS + DATA
+console.log("me gusta mcuho la peli"+ movies[1].movie+"dirigida por" +movies[1].director[0]
+)
+
+
+//template literal ==== ECMA script 6
+console.log(`NEW WAY === Me gusta mucho la peli ${movies[1].movie} dirigida por ${movies[1].director[0]}
+`);
+
+
